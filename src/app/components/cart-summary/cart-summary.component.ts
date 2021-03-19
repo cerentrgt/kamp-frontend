31f5476e9caf464c318589c4cd4d42b24d4f28dc
Ceren Turgut <veyselceren25@gmail.com>
2021-03-19 15:58:54 +0300

    19.gün yayın sonu..

---
+++ src/app/components/cart-summary/cart-summary.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { runInThisContext } from 'node:vm';
+import { CartItem } from 'src/app/models/cartitem';
+import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart-summary',
+  templateUrl: './cart-summary.component.html',
+  styleUrls: ['./cart-summary.component.css']
+})
+export class CartSummaryComponent implements OnInit {
+
+  cartItems:CartItem[]=[];
+  constructor(private cartService:CartService,
+    private toastrService:ToastrService) { }
+
+  ngOnInit(): void {
+    this.getCart();
+  }
+
+  getCart(){ 
+    this.cartItems=this.cartService.list();
+
+  }
+  removeFromCart(product:Product){
+    this.cartService.removeFromCart(product);
+    this.toastrService.error(product.productName+"sepetten silindi.");
+
+  }
+
+}
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,7 +14,11 @@ export class ProductComponent implements OnInit {
 
   products:Product[] = [];
   dataLoaded=false;
-  constructor(private productService:ProductService,private activatedRoute:ActivatedRoute) { }
+  filterText="";
+  constructor(private productService:ProductService,
+    private activatedRoute:ActivatedRoute,
+    private toastrService:ToastrService,
+    private cartService:CartService) { }
 
   ngOnInit(): void {
     
@@ -37,6 +43,11 @@ export class ProductComponent implements OnInit {
       this.products=response.data
       this.dataLoaded=true;
    })
+  }
+
+  addToCart(product:Product){
+    this.toastrService.success("Sepete eklendi",product.productName)
+    this.cartService.addToCart(product);
   }
 
 }
